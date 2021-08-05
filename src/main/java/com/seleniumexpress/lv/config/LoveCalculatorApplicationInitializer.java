e09e254f63bc5b0586c7df580f14be9e0b103e8b
Amandeep266 <52690673+Amandeep266@users.noreply.github.com>
2021-08-05 22:50:31 +0530

    Update LoveCalculatorApplicationInitializer.java

--- src/main/java/com/seleniumexpress/lv/config/LoveCalculatorApplicationInitializer.java
+++ src/main/java/com/seleniumexpress/lv/config/LoveCalculatorApplicationInitializer.java
@@ -11,21 +11,18 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class LoveCalculatorApplicationInitializer implements WebApplicationInitializer {
 
 	public void onStartup(ServletContext sc) throws ServletException {
-////		XmlWebApplicationContext wac=new XmlWebApplicationContext();
-////		wac.setConfigLocation("/WEB-INF/application-config.xml");  this class is used when our configuration  is in xml file
+
 	
-	//creating a container
 		AnnotationConfigWebApplicationContext webapplicationcontext=new AnnotationConfigWebApplicationContext();
 		
 		webapplicationcontext.register(LoveCalculatorAppConfig.class);
 		
-		
-		// create a dispatcher servlet object
-		
 		DispatcherServlet ds= new DispatcherServlet(webapplicationcontext);
-		//Register dispatcher servlet with servlet context
+		
 		ServletRegistration.Dynamic myCustomDsServlet= sc.addServlet("myServlet",ds);		
+		
 		myCustomDsServlet.setLoadOnStartup(1);
+		
 		myCustomDsServlet.addMapping("/");
 		
 		
